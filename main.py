8ee1a0f5e4fec205e1fb2e9e707dd4e2426c92f3
nhall105 <104880184+nhall105@users.noreply.github.com>
2022-05-11 08:44:58 -0400

    Added more type of conversions + notes in code

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class UnitConverter(EasyFrame):
         # Input panel creation
         inputPanel = self.addPanel(row = 1, column = 0, background = "purple")
                 
-        # Conversion input/output fields
+        # Conversion input field/output text (Also labels)
         inputPanel.addLabel("Input", row = 0, column = 0, sticky = tkinter.E)
         inputPanel.addLabel("Output", row = 0, column = 1, sticky = tkinter.E)
         self.inNum = inputPanel.addFloatField(" ", row = 1, column = 0)
@@ -28,9 +28,14 @@ class UnitConverter(EasyFrame):
 
         # Convert button because float fields can run commands smh
         self.conButton = inputPanel.addButton(text = "Convert", column = 0, row = 3, columnspan = 2, command = self.convert)
-        
+
+    # Function that grabs text in Unit Type drop-down and fills the Unit drop-downs accordingly    
     def typeGet(self):
+        
+        # Get text in Unit Type drop-down
         text = self.CT.getText()
+        
+        # Find matching text and fill Unit drop-downs accordingly
         if text == "Area":
             self.C1["values"] = ["Square kilometer", "Square meter", "Square mile", "Square yard", "Square foot", "Square inch", "Hectare", "Acre"]
             self.C2["values"] = ["Square kilometer", "Square meter", "Square mile", "Square yard", "Square foot", "Square inch", "Hectare", "Acre"]
@@ -41,17 +46,17 @@ class UnitConverter(EasyFrame):
             self.C1["values"] = ["Bit", "Kilobit", "Kibibit", "Megabit", "Mebibit", "Gigabit", "Gibibit", "Terabit", "Tebibit", "Petabit", "Pebibit", "Byte", "Kilobyte", "Kibibyte", "Megabyte", "Mebibyte", "Gigabyte", "Gibibyte", "Terabyte", "Tebibyte", "Petabyte", "Pebibyte"]
             self.C2["values"] = ["Bit", "Kilobit", "Kibibit", "Megabit", "Mebibit", "Gigabit", "Gibibit", "Terabit", "Tebibit", "Petabit", "Pebibit", "Byte", "Kilobyte", "Kibibyte", "Megabyte", "Mebibyte", "Gigabyte", "Gibibyte", "Terabyte", "Tebibyte", "Petabyte", "Pebibyte"]
         elif text == "Energy":
-            self.C1["values"] = ["DS 1"]
-            self.C2["values"] = ["DS 2"]
+            self.C1["values"] = ["Joule", "Kilojoule", "Gram calorie", "Kilocalorie", "Watt hour", "Kilowatt hour", "Electronvolt", "British thermal unit", "US therm", "Foot-pound"]
+            self.C2["values"] = ["Joule", "Kilojoule", "Gram calorie", "Kilocalorie", "Watt hour", "Kilowatt hour", "Electronvolt", "British thermal unit", "US therm", "Foot-pound"]
         elif text == "Frequency":
-            self.C1["values"] = ["DS 1"]
-            self.C2["values"] = ["DS 2"]
+            self.C1["values"] = ["Hertz", "Kilohertz", "Megahertz", "Gigahertz"]
+            self.C2["values"] = ["Hertz", "Kilohertz", "Megahertz", "Gigahertz"]
         elif text == "Fuel Economy":
-            self.C1["values"] = ["DS 1"]
-            self.C2["values"] = ["DS 2"]
+            self.C1["values"] = ["Miles per gallon", "Miles per gallon (Imperial)", "Kilometer per liter", "Liter per 100 kilometers"]
+            self.C2["values"] = ["Miles per gallon", "Miles per gallon (Imperial)", "Kilometer per liter", "Liter per 100 kilometers"]
         elif text == "Length":
-            self.C1["values"] = ["DS 1"]
-            self.C2["values"] = ["DS 2"]
+            self.C1["values"] = ["Kilometer", "Meter", "Centimeter", "Millimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot", "Inch", "Nautical mile"]
+            self.C2["values"] = ["Kilometer", "Meter", "Centimeter", "Millimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot", "Inch", "Nautical mile"]
         elif text == "Mass":
             self.C1["values"] = ["DS 1"]
             self.C2["values"] = ["DS 2"]
@@ -74,21 +79,34 @@ class UnitConverter(EasyFrame):
             self.C1["values"] = ["DS 1"]
             self.C2["values"] = ["DS 2"]
 
+    # Grabs the Unit drop-downs and number entered and convert, then outputs
     def convert(self):
+        
+        # Grab Units and number to convert
         unit1 = self.C1.getText()
         unit2 = self.C2.getText()
         num = float(self.inNum.getNumber())
+        
+        # Run drop-down text through function to get coresponding number
         unit1Num = units.unitValue(unit1)
         unit2Num = units.unitValue(unit2)
-        print(str(unit1Num))
-        print(str(unit2Num))
-        print(str(num))
+        
+        #print(str(unit1Num)) #Testing purposes
+        #print(str(unit2Num)) #Testing purposes
+        #print(str(num)) #Testing purposes
+
+        # Run numbers through conversion equation (uses a base unit for each meausurement so it takes current units, converts to base unit, then into final unit)
         convertNum = (num / unit1Num) * unit2Num
+        
+        # Tells how many decimal places based on number (turns into int if none)
         if float(convertNum).is_integer() == True:
             convertNum = int(convertNum)
         else:
             convertNum = max('{:.1f}'.format(convertNum),str(convertNum),key=len)
-        print(str(convertNum))
+
+        #print(str(convertNum)) #Testing purposes
+
+        # Put final number into text field
         self.outNum.setText(convertNum)
 
 def main():
--- units.py
+++ units.py
@@ -1,6 +1,8 @@
 # Conversion function to keep my main file clean-ish
 
 def unitValue(unit):
+
+    # Area units
     if unit == "Square kilometer":
         num = 1 / 1.076e+7
     elif unit == "Square meter":
@@ -17,6 +19,8 @@ def unitValue(unit):
         num = 1 / 107639
     elif unit == "Acre":
         num = 1 / 43560
+
+    # DTR units
     elif unit == "Bit per second":
         num = 1
     elif unit == "Kilobit per second":
@@ -43,6 +47,8 @@ def unitValue(unit):
         num = 1 / 8e+12
     elif unit == "Tebibit per second":
         num = 1 / (1024 * 1024 * 1024 * 1024)
+
+    # DS units
     elif unit == "Bit":
         num = 8
     elif unit == "Kilobit":
@@ -87,5 +93,74 @@ def unitValue(unit):
         num = 1 / 1e+15
     elif unit == "Pebibyte":
         num = 1 / (1024 ** 5)
+
+    # Energy units
+    elif unit == "Joule":
+        num = 1
+    elif unit == "Kilojoule":
+        num = 1 / 1000
+    elif unit == "Gram calorie":
+        num = 1 / 4.184
+    elif unit == "Kilocalorie":
+        num = 1 / 4184
+    elif unit == "Watt hour":
+        num = 1 / 3600
+    elif unit == "Kilowatt hour":
+        num = 1 / 3.6e+6
+    elif unit == "Electronvolt":
+        num = 6241506363094000000
+    elif unit == "British thermal unit":
+        num = 9.478169879e-9
+    elif unit == "US therm":
+        num = 9.480434279e-9
+    elif unit == "Foot-pound":
+        num = 0.7375621493
+
+    # Frequency units
+    elif unit == "Hertz":
+        num = 1
+    elif unit == "Kilohertz":
+        num = 1 / 1000
+    elif unit == "Megahertz":
+        num = 1 / 1e+6
+    elif unit == "Gigahertz":
+        num = 1 / 1e+9
+
+    # FE units
+    elif unit == "Miles per gallon":
+        num = 2.3521458329
+    elif unit == "Miles per gallon (Imperial)":
+        num = 2.8248093628
+    elif unit == "Kilometer per liter":
+        num = 1
+    elif unit == "Liter per 100 kilometers":
+        num = 100
+
+    # Length units
+    elif unit == "Kilometer":
+        num = 0.0003048
+    elif unit == "Meter":
+        num = 0.3048
+    elif unit == "Centimeter":
+        num = 30.48
+    elif unit == "Millimeter":
+        num = 304.8
+    elif unit == "Micrometer":
+        num = 304800
+    elif unit == "Nanometer":
+        num = 3.048e+8
+    elif unit == "Mile":
+        num = 1 / 5280
+    elif unit == "Yard":
+        num = 1 / 3
+    elif unit == "Foot":
+        num = 1
+    elif unit == "Inch":
+        num = 12
+    elif unit == "Nautical mile":
+        num = 0.0001645788
+
+    # Mass units
+    
 
     return num
